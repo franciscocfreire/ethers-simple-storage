f1ec81e0eef81ace6963b7760c55f9af103ee3a5
Francisco Freire <franciscocfreire@gmail.com>
2022-10-05 19:17:22 -0300

    typescript version

--- deploy.ts
+++ deploy.ts
@@ -1,9 +1,13 @@
 // synchronous [solidity]
 // asynchro
 
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+// const ethers = require("ethers");
+// const fs = require("fs-extra");
+// require("dotenv").config();
+
+import {ethers} from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config";
 
 async function main() {
     // solc can do
@@ -14,9 +18,9 @@ async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
     //const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
     const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    let wallet = ethers.Wallet.fromEncryptedJsonSync(
         encryptedJson,
-        process.env.PRIVATE_KEY_PASSWORD
+        process.env.PRIVATE_KEY_PASSWORD!
     );
 
     wallet = await wallet.connect(provider);
