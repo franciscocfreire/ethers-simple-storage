2e417e4dd8f74b6460021c2b12a7ae7778d8ce50
Francisco Freire <franciscocfreire@gmail.com>
2022-10-05 04:51:27 -0300

    create ENVIRONMENT VARIABLES

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@
 
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main() {
   // solc can do
@@ -10,13 +11,8 @@ async function main() {
   // compile them separetly
   // http://127.0.0.1:8545
 
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
-  const wallet = new ethers.Wallet(
-    "43d9c9db247a79d288ac11c793cfcb702aa9ae6c26c12083d689f7ae10287b13",
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
   const binary = fs.readFileSync(
